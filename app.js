bcfebe96b7a506b17abb03fde0615736d41f47e0
DESKTOP-C2QNSMF\youn <sho801921@gmail.com>
2022-03-05 16:45:13 +0900

    commit first

---
+++ app.js
@@ -0,0 +1,9 @@
+const firNum = 11;
+const secNum = 9;
+
+const myName = 'joey'
+
+console.log(firNum + secNum);
+console.log(firNum * secNum);
+console.log(firNum / secNum);
+console.log(myName+"likes"+secNum);
